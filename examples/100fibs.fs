916a7fe7fb190086bbab128e8ace5ad23cea8f22
joe <joseph.dougherty@gmail.com>
2017-10-14 19:04:47 -0400

    100/10 fibs

--- examples/100fibs.fs
+++
@@ -1,5 +0,0 @@
-: nextfib ( add new fib num to top of stack ) over over + ;
-: 100fibs ( produce first 100 fibs ) 1 1 99 1 ?DO nextfib LOOP ;
-
-100fibs 
-.
---
+++ examples/10fibs.fs
@@ -0,0 +1,5 @@
+: nextfib ( add new fib num to top of stack ) over over + ;
+: 10fibs ( produce first 100 fibs ) 1 1 9 1 ?DO nextfib LOOP ;
+
+10fibs 
+.

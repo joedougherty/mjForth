b03d892b1e9526914e6cca2e30e97849fb86c637
Joe Dougherty <joseph.dougherty@gmail.com>
2017-11-06 11:32:52 -0500

    Adds scaffolding for a sort of stdlib

---
+++ lib/stdlib.fs
@@ -0,0 +1,1 @@
+: range ( Generate a list of ints starting at 0 ) ?DO i 0= IF [ 0 ] ELSE i concat ENDIF LOOP ;
--- mjForth.py
+++ mjForth.py
@@ -275,7 +275,7 @@ def tokenize(input_line):
            replace(')', ' )').  \
            replace(';', ' ; '). \
            split(' ')
-    
+
     return [w for w in input_line if w != '']
 
 

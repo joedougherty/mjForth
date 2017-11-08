db4248b28ecce4d8be7cf4abb22a48b9ba2b7e34
joe <joseph.dougherty@gmail.com>
2017-11-08 18:36:13 -0500

    Re-worked range to handle arbitrary (in) to, from

--- lib/stdlib.fs
+++ lib/stdlib.fs
@@ -1,1 +1,1 @@
-: range ( Generate a list of ints starting at 0 ) ?DO i 0= IF [ 0 ] ELSE i concat ENDIF LOOP ;
+: range ( Produce a list of ints by [ to from ] inclusive ) [ ] swap i ?DO i concat LOOP ;

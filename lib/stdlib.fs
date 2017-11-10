d52791d49b9ebb3808d22cf80bb5c98316869889
joe <joseph.dougherty@gmail.com>
2017-11-10 12:53:08 -0500

    Support range word and range_c combinator

--- lib/stdlib.fs
+++ lib/stdlib.fs
@@ -1,2 +1,3 @@
-: range ( Produce a list of ints by [ to from ] inclusive ) [ ] swap i ?DO i concat LOOP ;
+: range ( Produce a list of ints by to, from *exclusive* ) [ ] concat concat [ ] swap i ?DO i concat LOOP ;
+: range_c ( Produce a list of ints by [ to from ] *exclusive* ) [ ] swap i ?DO i concat LOOP ;
 : square ( Multiply a number by itself ) dup * ;

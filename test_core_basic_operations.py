8b4c6a787e26bf294128cf72b92fe6c2d929075b
Joe Dougherty <joseph.dougherty@gmail.com>
2020-12-05 12:18:44 -0500

    tests

--- test_core_basic_operations.py
+++ test_core_basic_operations.py
@@ -24,3 +24,16 @@ def test_drop():
     tiny.push(1)
     drop(Data=tiny)
     assert(tiny.peek() == 'a')
+
+    empty = Stack()
+    with pytest.raises(IndexError):
+        drop(Data=empty)
+
+
+def test_swap():
+    two_items = Stack()
+    two_items.push('frick')
+    two_items.push('frack')
+    swap(Data=two_items)
+    assert(two_items.peek() == 'frick')
+    assert(two_items.contents[-2] == 'frack')

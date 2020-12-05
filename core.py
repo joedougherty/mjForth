76845dad528e03fdae7f0ef4337e4480fdb12910
Joe Dougherty <joseph.dougherty@gmail.com>
2020-12-05 11:13:03 -0500

    restructure

--- core.py
+++ core.py
@@ -1,10 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from Stack import Stack
-
 from copy import copy
 import operator
 import sys
+
+
+class Stack:
+    def __init__(self, height_limit=None):
+        self.contents = []
+        self.height_limit = height_limit
+
+    def push(self, item):
+        if self.height_limit:
+            if (self.height() + 1) > self.height_limit:
+                msg = "This Stack has a height_limit set to: {}".format(
+                    self.height_limit
+                )
+                raise ValueError(msg)
+        self.contents.append(item)
+
+    def pop(self):
+        return self.contents.pop()
+
+    def peek(self):
+        return self.contents[-1]
+
+    def is_empty(self):
+        return self.height() == 0
+
+    def height(self):
+        return len(self.contents)
+
 
 TRUE = -1
 FALSE = 0
